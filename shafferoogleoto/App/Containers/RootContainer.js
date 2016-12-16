64cbf90b7d1b0d52d509cdf7b1fb3669f2b4cde8
Ted Shaffer <ted@brightsign.biz>
2016-12-15 18:44:43 -0800

    improved UI

--- shafferoogleoto/App/Containers/RootContainer.js
+++ shafferoogleoto/App/Containers/RootContainer.js
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   picker: {
-    width: 100,
+    width: 400,
   }
 });
 
@@ -43,7 +43,20 @@ const Item = Picker.Item;
 
 class RootContainer extends Component {
 
+  state = {
+    selectedAlbum: ''
+  };
 
+  componentWillMount() {
+
+    let promise = this.fetchAlbums();
+    promise.then( (response) => {
+      this.parseAlbumsFeedResponse(response.data.feed);
+    }, (reason) => {
+      console.log("promise.then failed", reason);
+    });
+  }
+  
   fetchAlbums() {
     let serverURL = "http://localhost:8080/fetchAlbums";
 
@@ -56,142 +69,151 @@ class RootContainer extends Component {
 
   parseAlbumsFeedResponse(feed) {
 
-    for (let album of feed.entry) {
-      console.log("Album: ", album.title[0]._);
+    for (let googleAlbum of feed.entry) {
+      console.log("Album: ", googleAlbum.title[0]._);
 
-      const title = album.title[0]._;
-      const id = album["gphoto:id"][0];
-      const numPhotos = album["gphoto:numphotos"][0];
-      const timestamp = album["gphoto:timestamp"][0];
-      const mediaGroup = album["media:group"][0];
-      const published = album["published"][0];
-      const dateTimeUpdated = album["updated"][0];
-
-      console.log(title);
-      console.log(id);
-      console.log(numPhotos);
-      console.log(timestamp);
-      console.log(mediaGroup);
-      console.log(published);
-      console.log(dateTimeUpdated);
+      let album = {};
+      album.title = googleAlbum.title[0]._;
+      album.id = googleAlbum["gphoto:id"][0];
+      album.numPhotos = googleAlbum["gphoto:numphotos"][0];
+      album.timestamp = googleAlbum["gphoto:timestamp"][0];
+      album.mediaGroup = googleAlbum["media:group"][0];
+      album.published = googleAlbum["published"][0];
+      album.dateTimeUpdated = googleAlbum["updated"][0];
 
       this.props.addAlbum(album);
     }
   }
 
 
-  handleListAlbums() {
+  // handleListAlbums() {
+  //
+  //   console.log("handleListAlbums invoked");
+  //   let promise = this.fetchAlbums();
+  //   promise.then( (response) => {
+  //     this.parseAlbumsFeedResponse(response.data.feed);
+  //   }, (reason) => {
+  //     console.log("promise.then failed", reason);
+  //   });
+  // }
 
-    console.log("handleListAlbums invoked");
-    let promise = this.fetchAlbums();
-    promise.then( (response) => {
-      this.parseAlbumsFeedResponse(response.data.feed);
-    }, (reason) => {
-      console.log("promise.then failed", reason);
-    });
+
+  // fetchAlbum(albumId) {
+  //   let serverURL = "http://localhost:8080/fetchAlbum";
+  //
+  //   return new Promise( (resolve, reject) => {
+  //     axios.get(serverURL, {
+  //       params: { albumId }
+  //     }).then( (response) => {
+  //       resolve(response);
+  //     });
+  //   })
+  // }
+
+  // parseAlbumPhoto(photo) {
+  //
+  //   console.log("photo: ", photo.title[0]._);
+  //
+  //   // photoInfo will include:
+  //   //    height
+  //   //    medium
+  //   //    type
+  //   //    url
+  //   //    width
+  //   const photoInfo = photo["media:group"][0]["media:content"][0].$;
+  //
+  //   // thumbnails for photo in the array
+  //   //    photo["media:group"][0]["media:thumbnail"]
+  //   console.log(photoInfo);
+  // }
+
+  // parseAlbumFeedResponse(feed) {
+  //   console.log("Parse album: ", feed.title[0]._);
+  //   console.log("Last updated: ", feed.updated[0]);
+  //   console.log("Number of photos in the album is: ", Number(feed["gphoto:numphotos"][0]));
+  //   feed.entry.forEach( (photo) => {
+  //     this.parseAlbumPhoto(photo);
+  //   })
+  // }
+  //
+  // handleFetchAlbum() {
+  //   console.log("handleFetchAlbum invoked");
+  //   let promise = this.fetchAlbum("6157781148956998593");
+  //   promise.then( (response) => {
+  //     this.parseAlbumFeedResponse(response.data.feed);
+  //   }, (reason) => {
+  //     console.log("promise.then failed", reason);
+  //   });
+  // }
+
+  handleStartSlideShow() {
+    console.log("handleSlideShow for album: ", this.state.selectedAlbum);
   }
-
-
-  fetchAlbum(albumId) {
-    let serverURL = "http://localhost:8080/fetchAlbum";
-
-    return new Promise( (resolve, reject) => {
-      axios.get(serverURL, {
-        params: { albumId }
-      }).then( (response) => {
-        resolve(response);
-      });
-    })
-  }
-
-  parseAlbumPhoto(photo) {
-    console.log("")
-
-    console.log("photo: ", photo.title[0]._);
-
-    // photoInfo will include:
-    //    height
-    //    medium
-    //    type
-    //    url
-    //    width
-    const photoInfo = photo["media:group"][0]["media:content"][0].$;
-
-    // thumbnails for photo in the array
-    //    photo["media:group"][0]["media:thumbnail"]
-    console.log(photoInfo);
-
-  }
-  parseAlbumFeedResponse(feed) {
-    console.log("Parse album: ", feed.title[0]._);
-    console.log("Last updated: ", feed.updated[0]);
-    console.log("Number of photos in the album is: ", Number(feed["gphoto:numphotos"][0]));
-    feed.entry.forEach( (photo) => {
-      this.parseAlbumPhoto(photo);
-    })
-  }
-
-  handleFetchAlbum() {
-    console.log("handleFetchAlbum invoked");
-    let promise = this.fetchAlbum("6157781148956998593");
-    promise.then( (response) => {
-      this.parseAlbumFeedResponse(response.data.feed);
-    }, (reason) => {
-      console.log("promise.then failed", reason);
-    });
-  }
-
-  state = {
-    selected1: 'key1',
-    selected2: 'key1',
-    selected3: 'key1',
-    color: 'red',
-    mode: Picker.MODE_DIALOG,
-  };
-
-  // onValueChange = (key: string, value: string) => {
+  
   onValueChange = (value: string, itemPosition) => {
     console.log("selected value: ", value);
     console.log("itemPosition: ", itemPosition);
-    // const newState = {};
-    // newState[key] = value;
-    // this.setState(newState);
-    let newState = { selected1: value };
+    let newState = { selectedAlbum: value };
     this.setState(newState);
   };
 
+  buildAlbumRow(album) {
+
+    return (
+      <Item
+        label={album.title}
+        value={album.id}
+        key={album.id}
+      />
+    );
+  }
+
+  buildAlbumList() {
+
+    var self = this;
+
+    let albumRows = this.props.albums.map(function(album) {
+      const albumRow = self.buildAlbumRow(album);
+      return albumRow;
+    });
+    return albumRows;
+  }
+
   render() {
+
+    let albumsJSX;
+    if (this.props.albums.length === 0) {
+      albumsJSX = <noscript/>;
+    }
+    else {
+      albumsJSX = this.buildAlbumList();
+    }
+
+    // <Button
+    //   onPress={this.handleListAlbums.bind(this)}
+    //   title="List Albums"
+    //   color="#841584"
+    // />
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Shafferoogleoto!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          Albums
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Button
-          onPress={this.handleListAlbums.bind(this)}
-          title="List Albums"
-          color="#841584"
-        />
-        <Button
-          onPress={this.handleFetchAlbum.bind(this)}
-          title="Fetch Album"
-          color="#841584"
-        />
         <Picker
           style={styles.picker}
-          selectedValue={this.state.selected1}
+          selectedValue={this.state.selectedAlbum}
           onValueChange={this.onValueChange.bind(this)}>
-          <Item label="choice1" value="key0" />
-          <Item label="choice2" value="key1" />
-          <Item label="choice3" value="key2" />
-          <Item label="choice4" value="key3" />
+          {albumsJSX}
         </Picker>
+        <Button
+          onPress={this.handleStartSlideShow.bind(this)}
+          title="Start Slide Show"
+          color="#841584"
+        />
       </View>
     );
   }
@@ -200,10 +222,9 @@ class RootContainer extends Component {
 
 // {/*onValueChange={this.onValueChange.bind(this, 'selected1')}>*/}
 
-// const mapStateToProps = (state) => ({
-//   app: state.app,
-//   recentFiles: state.app.recentFiles
-// });
+const mapStateToProps = (state) => ({
+  albums: state.albums,
+});
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
@@ -211,4 +232,4 @@ const mapDispatchToProps = (dispatch) => {
   }, dispatch);
 };
 
-export default connect(null, mapDispatchToProps)(RootContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(RootContainer);
