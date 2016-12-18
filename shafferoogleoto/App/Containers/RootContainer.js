840fa3ee14acf93ccdffa2e1de13219762a89702
Ted Shaffer <ted@brightsign.biz>
2016-12-18 09:43:34 -0800

    UI to connect to BrightPhoto device.

--- shafferoogleoto/App/Containers/RootContainer.js
+++ shafferoogleoto/App/Containers/RootContainer.js
@@ -10,6 +10,7 @@ import {
   Picker,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 
@@ -32,8 +33,30 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   instructions: {
+    textAlign: 'left',
+    fontSize: 14,
+    color: '#111111',
+    marginBottom: 2,
+  },
+  ipAddressTextInput: {
+    height: 24,
+    borderWidth: 0.5,
+    borderColor: '#0f0f0f',
+    color: '#111111',
+    fontSize: 12,
+    padding: 2,
+    marginLeft: 4
+  },
+  // connectToBrightPhotoButton: {
+  //   height: 24,
+  //   // color: '#111111',
+  //   fontSize: 12,
+  //   padding: 1,
+  // },
+  header: {
     textAlign: 'center',
     color: '#333333',
+    fontSize: 18,
     marginBottom: 5,
   },
 });
@@ -42,6 +65,9 @@ class RootContainer extends Component {
 
   constructor(props) {
     super(props);
+
+    this.brightPhotoIPAddress = "192.168.0.101";
+
     this.selectedAlbum = null;
 
     // this.serverUrl = "http://localhost:8080/";
@@ -89,8 +115,17 @@ class RootContainer extends Component {
 
   }
 
-  launchSlideShow(albumId) {
+  handleConnectToBrightPhoto() {
+    console.log("handleConnectToBrightPhoto, connect to: ");
+    console.log(this.brightPhotoIPAddress);
+  }
 
+  handleSelectAlbum(album) {
+    console.log("handleSelectAlbum invoked for album: ", album);
+    this.selectedAlbum = album;
+  }
+
+  launchSlideShow(albumId) {
     return new Promise((resolve, reject) => {
       const url = this.serverUrl + "launchSlideShow";
       axios.get(url, {
@@ -101,66 +136,6 @@ class RootContainer extends Component {
     });
   }
 
-  // handleListAlbums() {
-  //
-  //   console.log("handleListAlbums invoked");
-  //   let promise = this.fetchAlbums();
-  //   promise.then( (response) => {
-  //     this.parseAlbumsFeedResponse(response.data.feed);
-  //   }, (reason) => {
-  //     console.log("promise.then failed", reason);
-  //   });
-  // }
-
-
-  // fetchAlbum(albumId) {
-  //   let serverURL = "http://localhost:8080/fetchAlbum";
-  //
-  //   return new Promise( (resolve, reject) => {
-  //     axios.get(serverURL, {
-  //       params: { albumId }
-  //     }).then( (response) => {
-  //       resolve(response);
-  //     });
-  //   })
-  // }
-
-  // parseAlbumPhoto(photo) {
-  //
-  //   console.log("photo: ", photo.title[0]._);
-  //
-  //   // photoInfo will include:
-  //   //    height
-  //   //    medium
-  //   //    type
-  //   //    url
-  //   //    width
-  //   const photoInfo = photo["media:group"][0]["media:content"][0].$;
-  //
-  //   // thumbnails for photo in the array
-  //   //    photo["media:group"][0]["media:thumbnail"]
-  //   console.log(photoInfo);
-  // }
-
-  // parseAlbumFeedResponse(feed) {
-  //   console.log("Parse album: ", feed.title[0]._);
-  //   console.log("Last updated: ", feed.updated[0]);
-  //   console.log("Number of photos in the album is: ", Number(feed["gphoto:numphotos"][0]));
-  //   feed.entry.forEach( (photo) => {
-  //     this.parseAlbumPhoto(photo);
-  //   })
-  // }
-  //
-  // handleFetchAlbum() {
-  //   console.log("handleFetchAlbum invoked");
-  //   let promise = this.fetchAlbum("6157781148956998593");
-  //   promise.then( (response) => {
-  //     this.parseAlbumFeedResponse(response.data.feed);
-  //   }, (reason) => {
-  //     console.log("promise.then failed", reason);
-  //   });
-  // }
-
   handleStartSlideShow() {
     console.log("handleStartSlideShow invoked");
     if (this.selectedAlbum) {
@@ -168,18 +143,51 @@ class RootContainer extends Component {
     }
   }
 
-  handleSelectAlbum(album) {
-    console.log("handleSelectAlbum invoked for album: ", album);
-    this.selectedAlbum = album;
-  }
+  /*
+      Failed button styling
+   height="16px"
+   fontSize="6px"
+   padding="1"
+   */
+  // defaultValue="192.168.0.101"
 
   render() {
+
+    let self = this;
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to Shafferoogleoto!
         </Text>
         <Text style={styles.instructions}>
+          Enter BrightPhoto IP address
+        </Text>
+        <TextInput
+          defaultValue="192.168.0.101"
+          autoFocus={true}
+          style={styles.ipAddressTextInput}
+          onChange={(event) => {
+            self.brightPhotoIPAddress = event.nativeEvent.text;
+            console.log('onChange text: ' + event.nativeEvent.text);
+          }}
+          onEndEditing={(event) => {
+            self.brightPhotoIPAddress = event.nativeEvent.text;
+            console.log('onEndEditing text: ' + event.nativeEvent.text);
+          }}
+          onSubmitEditing={(event) => {
+            self.brightPhotoIPAddress = event.nativeEvent.text;
+            console.log('onSubmitEditing text: ' + event.nativeEvent.text);
+            self.handleConnectToBrightPhoto();
+          }}
+        />
+        <Button
+          onPress={this.handleConnectToBrightPhoto.bind(this)}
+          title="Connect to BrightPhoto"
+          color="#111111"
+        />
+
+        <Text style={styles.header}>
           Albums
         </Text>
         <AlbumPicker
